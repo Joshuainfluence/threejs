3cabbe78a1149e1664d4beadefec1ba2feb70604
unknown <joshuajulius2030@gmail.com>
2025-01-05 09:30:37 +0100

    camera postions and setup

--- basic/main.js
+++ basic/main.js
@@ -4,3 +4,6 @@ import * as THREE from 'three';
 const scene = new THREE.scene();
 scene.background = new THREE.Color('#F0F0F0');
 
+//Add the camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
