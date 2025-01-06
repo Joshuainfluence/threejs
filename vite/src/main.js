f1a2485bf0abdc3febb025fcf4d1cd4e8a8bf3ba
unknown <joshuajulius2030@gmail.com>
2025-01-06 10:58:20 +0100

    animation

--- vite/src/main.js
+++ vite/src/main.js
@@ -14,11 +14,11 @@ camera.position.z = 5;
 
 //object
 const geometry = new THREE.DodecahedronGeometry();
-const material = new THREE.MeshBasicMaterial({color: '#468585'});
+const material = new THREE.MeshLambertMaterial({color: '#468585', emissive: '#468585'});
 const dodecahedron = new THREE.Mesh(geometry, material);
 
 const boxGeometry = new THREE.BoxGeometry(2, 0.1, 2);
-const boxMaterial = new THREE.MeshBasicMaterial({color: '#468585'});
+const boxMaterial = new THREE.MeshStandardMaterial({color: '#B4B4B3', emissive: '#B4B4B3'});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 box.position.y = -1.5;
 
@@ -34,7 +34,6 @@ scene.add(light);
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
-renderer.render(scene, camera);
 
 // Add orbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -43,7 +42,20 @@ controls.dampingFactor = 0.05;
 controls.enableZoom = true;
 controls.enablePan = true;
 
+// Animations
+function animate(){
+    requestAnimationFrame(animate);
 
+    dodecahedron.rotation.x += 0.01;
+    dodecahedron.rotation.y += 0.01;
+
+    box.rotation.y += 0.005;
+    renderer.render(scene, camera);
+
+    controls.update();
+
+}
+animate();
 
 
 
