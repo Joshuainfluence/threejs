f7a77bf9fc37265259ba1ca0b71230a481a530e8
unknown <joshuajulius2030@gmail.com>
2025-01-08 02:18:46 +0100

    installed media pipe

--- vite-react/src/App.jsx
+++ vite-react/src/App.jsx
@@ -1,5 +1,24 @@
-import {Canvas} from "@react-three/fiber";
+import {Canvas, useFrame} from "@react-three/fiber";
 import {OrbitControls} from "@react-three/drei";
+import {useRef} from "react";
+
+const RotatingCube = () => {
+
+    const meshRef = useRef();
+
+    useFrame(()=> {
+        if (meshRef.current){
+            meshRef.current.rotation.y += 0.01
+            meshRef.current.rotation.x += 0.01
+        }
+    })
+    return (
+        <mesh ref={meshRef}>
+            <cylinderGeometry args={[1,1,1]} />
+            <meshLambertMaterial color="#46858" emissive="#468585" />
+        </mesh>
+    )
+}
 
 const App = () => {
     return (
@@ -8,6 +27,10 @@ const App = () => {
             <OrbitControls enableZoom enablePan enableRotate/>
 
             <directionalLight position={[1, 1, 1]} intensity={10} color={0x9CDBA6} />
+
+            <color attach="background" args={['#F0F0F0']} />
+
+            <RotatingCube />
         </Canvas>
     );
 }
