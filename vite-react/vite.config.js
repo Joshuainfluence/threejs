841ac50f5bc76fad4e8581895c0a8bd89ce855e9
unknown <joshuajulius2030@gmail.com>
2025-01-09 09:13:21 +0100

    fixing error

--- vite-react/vite.config.js
+++ vite-react/vite.config.js
@@ -4,27 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['@mediapipe/tasks-vision', 'hls.js']
-  },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      external: ['@mediapipe/tasks-vision', 'hls.js']
-    }
-  },
-  resolve: {
-    alias: {
-      'three-stdlib': 'three-stdlib/dist/three-stdlib.module.js'
-    }
-  },
-  // Disable source maps for problematic dependencies
-  css: {
-    devSourcemap: false
-  },
-  server: {
-    sourcemap: {
-      suppressSourcemapWarning: true
-    }
-  }
 })
